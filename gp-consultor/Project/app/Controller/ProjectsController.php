2bdd86035972cabf83c0194372de6f1da781b05c
emesonsantana <emesonsantana@gmail.com>
2013-01-11 02:35:47 +0000

    Método Edit e Delete Projeto.

--- gp-consultor/Project/app/Controller/ProjectsController.php
+++ gp-consultor/Project/app/Controller/ProjectsController.php
@@ -29,6 +29,38 @@
  			$this-> set ('companies',$this->Project->Company->find('all'));
  		}
  	}
+	
+	public function edit($id = NULL){
+		$this->layout = 'base';
+		$this->Project->id = $id;
+		if (!$id) {
+        	throw new NotFoundException(__('Invalid post'));
+	    }
+	
+	    $consult = $this->Project->findById($id);
+	    if (!$consult) {
+	        throw new NotFoundException(__('Invalid post'));
+	    }
+		if ($this->request->is('get')) {
+			$this->request->data = $this->Project->read();
+		} 
+		else {
+			$this->Project->id = $id;
+			if ($this->Project->saveAll($this->request->data)) {
+				
+				$this->Session->setFlash('Projeto foi editado.');
+				$this->redirect(array('action' => 'index'));
+			}
+		}
+	}
+	
+	public function delete($id = NULL){
+		$this->Project->id = $id;
+		if($this->Project->saveField("removed", "true")){
+			$this->Session->setFlash('O consultor foi deletado.');
+			$this->redirect(array('action' => 'index'));
+		}
+	}
  	
  	public function GetCompanies(){
  		$this-> set ('companies',$this->Project->find('all'));
