3ba991e5770155dc36dc37e4e961fc93c80f5a24
marcela.domingues <marcela.domingues@gmail.com>
2013-01-14 19:49:31 +0000

    Ajuste no menu home.

--- gp-consultor/Project/app/View/Layouts/includes/menu.php
+++ gp-consultor/Project/app/View/Layouts/includes/menu.php
@@ -1,5 +1,6 @@
 <div id="Menu_Home">
-	<a href="#" id="botao_home"><?php echo $this->Html->image('logo.png', array('alt' => 'sw'))?><?php echo $this->Html->image('botao_home.png', array('alt' => '', 'id'=>'botao_home_img'))?></a>
+	<a href="#" id="botao_home"><?php echo $this->Html->image('logo.png', array('alt' => 'sw', 'id'=>'logoSw'))?>
+		<?php echo $this->Html->image('botao_home.png', array('alt' => '', 'id'=>'botao_home_img'))?></a>
 		<ul>
 			<li><?php echo $this->Html->link('Consultor', array('action' =>'../consultants'));?></li>
 			<li><?php echo $this->Html->link('Empresa', array('action' =>'../companies'));?></li>
